36729a39ac79935ce2cf7536594185a9af53e48d
ciec-20171103189 <43473349+ciec-20171103189@users.noreply.github.com>
2019-06-18 11:59:44 +0800

    node
    f

--- scripts/Game.js
+++ scripts/Game.js
@@ -20,11 +20,52 @@ cc.Class({
         player: {
             default: null,
             type: cc.Node
-        }
+        },
+        
+        scoreDisplay: {
+        default: null,
+        type: cc.Label
+        },
     },
 
+    onLoad: function () {
+
+        this.groundY = this.ground.y + this.ground.height/2;
+
+        this.spawnNewStar();
+        
+        this.score = 0;
+    },
+
+    spawnNewStar: function() {
+    	
+        var newStar = cc.instantiate(this.starPrefab);
+
+        this.node.addChild(newStar);
+  
+        newStar.setPosition(this.getNewStarPosition());
+        
+        newStar.getComponent('Star').game = this;
+    },
+
+    getNewStarPosition: function () {
+        var randX = 0;
+
+        var randY = this.groundY + Math.random() * this.player.getComponent('Player').jumpHeight;
+
+        var maxX = this.node.width;
+        randX = Math.random() * maxX;
+
+        return cc.v2(randX, randY);
+    },
+    
     start () {
 
     },
 
+    gainScore: function () {
+        this.score += 1;
+
+        this.scoreDisplay.string = 'Score: ' + this.score;
+    },
 });
--- scripts/Star.js
+++ scripts/Star.js
@@ -7,8 +7,37 @@ cc.Class({
     	pickRadius: 0,
     },
 
+    getPlayerDistance: function () {
+
+        var playerPos = this.game.player.getPosition();
+
+        var dist = this.node.position.sub(playerPos).mag();
+        return dist;
+    },
+
+    onPicked: function() {
+    	
+        this.game.spawnNewStar();
+        
+        this.node.destroy();
+        
+        this.game.spawnNewStar();
+        
+        this.game.gainScore();
+        
+        this.node.destroy();
+    },
+    
     start () {
 
     },
 
+    update: function (dt) {
+  
+        if (this.getPlayerDistance() < this.pickRadius) {
+        	
+            this.onPicked();
+            return;
+        }
+    },
 });
